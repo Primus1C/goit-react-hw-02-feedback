38ece2cca079aedc338267f8604871175accbf14
Artur Rakhimov <kharkiv1c@gmail.com>
2023-08-12 19:01:00 +0200

    develop

---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './FeedbackOptions.css';
+
+const FeedbackOptions = (options = {} onLeaveFeedback = {}) { }
+
+export default FeedbackOptions
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,11 +1,25 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Statistics.css';
-import React from 'react';
 
 class Statistics extends React.Component {
-  handleIncrement = () => {
-    console.log(this);
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
   };
+
+  handleIncrement = (evt) => {
+    console.log(evt);
+    //this.setState(prevState => ({ point: prevState[point] + 1 }));
+  };
+
+  countTotalFeedback = () =>
+    this.state.good + this.state.neutral + this.state.bad;
+
+  countPositiveFeedbackPercentage = () =>
+    (this.state.good / this.countTotalFeedback) * 100;
+
   render() {
     return (
       <>
@@ -13,27 +27,35 @@ class Statistics extends React.Component {
         <div className="Statistics__buttons">
           <button
             type="button"
-            className="Statistics__buttons__button"
-            onClick={this.handleIncrement}
+            className="Statistics__buttons__button Statistics__buttons__button_good"
+            onClick={this.handleIncrement()}
           >
             Good
           </button>
-          <button type="button" className="Statistics__buttons__button">
+          <button
+            type="button"
+            className="Statistics__buttons__button Statistics__buttons__button_neutral"
+            onClick={this.handleIncrement()}
+          >
             Neutral
           </button>
-          <button type="button" className="Statistics__buttons__button">
+          <button
+            type="button"
+            className="Statistics__buttons__button Statistics__buttons__button_bad"
+            onClick={this.handleIncrement()}
+          >
             Bad
           </button>
         </div>
         <h3>Statistics</h3>
         <div>
-          Good: <span className="counterGood">0</span>
+          Good: <span className="counterGood">{this.state.good}</span>
         </div>
         <div>
-          Neutral: <span className="counterNeutral">0</span>
+          Neutral: <span className="counterNeutral">{this.state.neutral}</span>
         </div>
         <div>
-          Bad: <span className="counterBad">0</span>
+          Bad: <span className="counterBad">{this.state.bad}</span>
         </div>
         <div>
           Total feedbacks: <span className="counterTotal">0</span>
